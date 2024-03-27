f9f9773d8d2fc2775b585792076707b9cd5f1aea
Leedom <jld1221@163.com>
2024-03-28 00:42:04 +0800

    fix(github): only sponsoring tab will be effected

--- extension/scripts/github.js
+++ extension/scripts/github.js
@@ -1,8 +1,22 @@
+// window.onload = () => {
+//   const PAST_SPONSORSHIP = document.getElementsByClassName('Label--secondary')
+
+//   if (PAST_SPONSORSHIP?.length) {
+//     for (const dom of PAST_SPONSORSHIP) {
+//       dom.style.backgroundColor = '#bf3989'
+//       dom.style.border = '#bf3989'
+//       dom.style.color = '#fff'
+//     }
+//   }
+// }
+
 // 监听是否有新dom插入
 document.addEventListener('DOMNodeInserted', function() {
   const PAST_SPONSORSHIP = document.getElementsByClassName('Label--secondary')
 
-  if (PAST_SPONSORSHIP?.length) {
+  const search = window.location.search
+
+  if (PAST_SPONSORSHIP?.length && search === '?tab=sponsoring') {
     for (const dom of PAST_SPONSORSHIP) {
       dom.style.backgroundColor = '#bf3989'
       dom.style.border = '#bf3989'
