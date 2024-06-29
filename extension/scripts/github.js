71a90b79f006f65ed8a44101e3d47cf39c3d71c8
Leedom <jld1221@163.com>
2024-06-29 22:47:03 +0800

    feat(github): highlight following

--- extension/scripts/github.js
+++ extension/scripts/github.js
@@ -10,17 +10,40 @@
 //   }
 // }
 
-// 监听是否有新dom插入
-document.addEventListener('DOMNodeInserted', function () {
+function getParams(params) {
+  const search = window.location.search
+  const urlParams = new URLSearchParams(search)
+  return urlParams.get(params)
+}
+
+function highlightPastSponsorship() {
+  const search = window.location.search
   const PAST_SPONSORSHIP = document.getElementsByClassName('Label--secondary')
 
-  const search = window.location.search
-
-  if (PAST_SPONSORSHIP?.length && search === '?tab=sponsoring') {
+  if (PAST_SPONSORSHIP?.length && getParams('tab') === 'sponsoring') {
     for (const dom of PAST_SPONSORSHIP) {
       dom.style.backgroundColor = '#bf3989'
       dom.style.border = '#bf3989'
       dom.style.color = '#fff'
     }
   }
+}
+
+function highlightFollowing() {
+  const search = window.location.search
+  const elements = document.querySelectorAll('form[hidden="hidden"] input[name="commit"][value="Follow"]')
+
+  if (elements?.length && ['following', 'followers'].includes(getParams('tab'))) {
+    for (const dom of elements) {
+      const parent = dom.parentElement.closest('div.d-table')
+      parent.style.backgroundColor = 'wheat'
+      parent.style.padding = '15px'
+    }
+  }
+}
+
+// 监听是否有新dom插入
+document.addEventListener('DOMNodeInserted', function () {
+  highlightPastSponsorship()
+  highlightFollowing()
 })
